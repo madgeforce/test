a72d93d3fc5e7041c6a08e59bb485c65823ef334
madgeforce <129367912+madgeforce@users.noreply.github.com>
2023-06-13 18:01:55 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,42 +1,41 @@
 // next.config.js
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  // your pwa config
+  manifest: {
+    name: 'My Next.js App',
+    short_name: 'Next App',
+    description: 'A PWA with Next.js',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  },
+  workboxOpts: {
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200
+          }
+        }
+      }
+    ]
+  }
+})
 
 module.exports = withPWA({
   // your next.js config
-  pwa: {
-    // your pwa config
-    dest: 'public',
-    manifest: {
-      name: 'My Next.js App',
-      short_name: 'Next App',
-      description: 'A PWA with Next.js',
-      theme_color: '#ffffff',
-      icons: [
-        {
-          src: '/icon-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    },
-    workboxOpts: {
-      runtimeCaching: [
-        {
-          urlPattern: /^https?.*/,
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'offlineCache',
-            expiration: {
-              maxEntries: 200
-            }
-          }
-        }
-      ]
-    }
-  }
 })
