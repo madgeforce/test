14f3ea8f0dfa62402fd65b2d69dfcaf825a4e736
madgeforce <129367912+madgeforce@users.noreply.github.com>
2023-06-13 19:41:30 +0100

    mm

--- next.config.js
+++ next.config.js
@@ -1,23 +1,7 @@
 const withPWA = require('next-pwa')({
-  dest: 'public',
-  // your pwa config
-  swDest: 'sw.js', // specify the destination of the service worker file
-  workboxOpts: {
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          expiration: {
-            maxEntries: 200
-          }
-        }
-      }
-    ]
-  }
+  dest: 'public'
 })
 
 module.exports = withPWA({
-  // your next.js config
+  // next.js config
 })
