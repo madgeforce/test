b1221b8868342154f89973b02ee10fd2e02a3fcf
madgeforce <129367912+madgeforce@users.noreply.github.com>
2023-06-13 18:10:08 +0100

    nnn

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
-// next.config.js
 const withPWA = require('next-pwa')({
   dest: 'public',
   // your pwa config
-  manifest: {
-    name: 'My Next.js App',
-    short_name: 'Next App',
-    description: 'A PWA with Next.js',
-    theme_color: '#ffffff',
-    icons: [
-      {
-        src: '/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png'
-      },
-      {
-        src: '/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png'
-      }
-    ]
-  },
+  swDest: 'sw.js', // specify the destination of the service worker file
   workboxOpts: {
     runtimeCaching: [
       {
